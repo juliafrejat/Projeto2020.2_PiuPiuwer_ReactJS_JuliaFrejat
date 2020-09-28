f74a2772e2209d4d9d511019b2fbf9d4ce994a75
Julia <juliafrejat@gmail.com>
2020-09-28 12:11:54 -0300

    commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,6 +8,7 @@ import messageIcon from '../../assets/images/mensagem.svg';
 import notifIcon from '../../assets/images/notificacao.svg';
 import profileIcon from '../../assets/images/perfil.svg';
 import moreIcon from '../../assets/images/mais.svg';
+import searchIcon from '../../assets/images/busca.svg';
 
 interface PageHeaderProps {
     hasMenu: boolean;
@@ -18,20 +19,25 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
     if (hasMenu){
         return (
             <header className="page-header">
-                <div className="logo-container">
+                <div className="container-row title">
                     <Link to="/">
                         <img src={logoImg} alt="PiuPiuwer" />
                         <h1>PiuPiuwer</h1>
                     </Link>
                 </div>
     
-                <div className="header-content">
+                <div className="container-row nav">
                     <Link to="/"><img src={homeIcon} alt="Página Inicial" /></Link>
-                    <Link to="/"><img src={exploreIcon} alt="Explorar" /></Link>
-                    <Link to="/"><img src={messageIcon} alt="Mensagens" /></Link>
-                    <Link to="/"><img src={notifIcon} alt="Notificações" /></Link>
-                    <Link to="/"><img src={profileIcon} alt="Perfil" /></Link>
+                    <Link to="/explore"><img src={exploreIcon} alt="Explorar" /></Link>
+                    <Link to="/messages"><img src={messageIcon} alt="Mensagens" /></Link>
+                    <Link to="/notifications"><img src={notifIcon} alt="Notificações" /></Link>
+                    <Link to="/profile"><img src={profileIcon} alt="Perfil" /></Link>
                     <Link to="/"><img src={moreIcon} alt="Mais" /></Link>
+                </div>
+
+                <div className="container-row search">
+                    <input type="text" name="search"/>
+                    <button type="submit"><img src={searchIcon} alt="Buscar"/></button>
                 </div>
             </header>
         )
