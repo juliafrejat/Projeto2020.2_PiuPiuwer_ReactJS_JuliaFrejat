1efaf2b00da89c0fb4309e2c6a0c8403df8150c8
Julia <juliafrejat@gmail.com>
2020-09-28 14:00:26 -0300

    comiit

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,1 +1,16 @@
 import React from 'react';
+
+import { FooterComponent } from './styles';
+
+interface FooterProps {
+
+}
+
+const Button: React.FC<FooterProps> = (props) => {
+    return (
+        <FooterComponent></FooterComponent>
+    )
+    
+}
+
+export default Button;
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,5 +1,17 @@
-import { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 
-export default createGlobalStyle`
+interface FooterProps {
+}
 
+export const FooterComponent = styled.div<FooterProps>`
+    width: 100%;
+    height: 5%;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    justify-content: center;
+    background-color: #ffffff;
+    border: 1px solid #CED4DA;
+    color: #95D5B2;
+    font-size: 15px;
 `;
